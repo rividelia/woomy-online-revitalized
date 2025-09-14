7fd1f6c30f577bd149e3925dd7d5415b8f78b4dd
Musicalement1 <musicalement.github@gmail.com>
2025-09-14 09:38:45 +0200

    Revert multiplayer.js commit that changes web url

--- client/js/multiplayer.js
+++ client/js/multiplayer.js
@@ -3,7 +3,7 @@ import { PeerWrapper } from "./peer.js";
 
 window.connectedToWRM = false
 
-const WRM = window.location.host==="localhost"?"localhost":"woomy-2rbx.onrender.com"
+const WRM = window.location.host==="localhost"?"localhost":"woomy.online"
 const wsUrl = window.location.protocol === "http:" ? "ws://" : "wss://"
 const httpUrl = window.location.protocol === "http:" ? "http://" : "https://"
 const WRM_WS = `${wsUrl}${WRM}`
